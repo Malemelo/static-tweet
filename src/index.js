3d96687fc112f0be6b19b4247e6b77ae32f8ac63
Isaac Malemelo <imalemelo@yahoo.com>
2021-04-23 13:03:27 +0200

    avatar component added

--- src/index.js
+++ src/index.js
@@ -3,12 +3,27 @@ import reactDom from 'react-dom';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Tweet(){
+/* const Tweet = () => {
     return (
         <div className="tweet">
             Tweet
         </div>
     );
+} */
+ const Avatar = () => {
+     return(
+        <img src="https://www.gravatar.com/avatar/nothing" className="avatar" alt="avatar"/>
+     );
+ }
+function Tweet(){
+    return (
+        <div className="tweet">
+            <Avatar/>
+            Tweet
+        </div>
+    );
 }
+
+
 
 reactDom.render(<Tweet/>, document.querySelector('#root'));
